f99926dc9846333a82c02fbb341d8280eb086c25
Mehmet Yıldız <myrevolver13@gmail.com>
2017-11-05 17:45:28 +0300

    Introduction to Java Programming, Page 131 Exercise **3.4

---
+++ src/RandomMonth/RandomMonth.java
@@ -0,0 +1,30 @@
+package RandomMonth;
+
+/**
+ * Created by Mehmet on 5.11.2017.
+ */
+public class RandomMonth {
+
+    public static void main(String[] args) {
+
+        int randomNumber = (int)(Math.random() * 12);
+
+        System.out.print("Random month : ");
+
+        switch (randomNumber){
+            case 0: System.out.println("January"); break;
+            case 1: System.out.println("February"); break;
+            case 2: System.out.println("March"); break;
+            case 3: System.out.println("April"); break;
+            case 4: System.out.println("May"); break;
+            case 5: System.out.println("June"); break;
+            case 6: System.out.println("July"); break;
+            case 7: System.out.println("August"); break;
+            case 8: System.out.println("September"); break;
+            case 9: System.out.println("October"); break;
+            case 10: System.out.println("November"); break;
+            case 11: System.out.println("December"); break;
+        }
+    }
+
+}
